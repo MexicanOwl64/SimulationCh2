a445c626f1b7c377bc00bc451f8757b048cd4d5e
MexicanOwl64 <55261799+MexicanOwl64@users.noreply.github.com>
2020-11-27 20:50:10 -0600

    Added exercise 2_4
    added the exercise 2_4 it should be working as intended

---
+++ SeriousGameCh2/Exercise2_4/Exercise2_4/Assets/Scripts/BoostRight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostRight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
